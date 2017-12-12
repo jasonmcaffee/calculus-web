5e3c6e6785c847b579851d1e92d0fc4fe3a5b58d
Jason Mcaffee <jason.mcaffee@insidesales.com>
2017-12-11 21:22:50 -0700

    wip - started refactor for MessageChannel

--- src/webworker/broadcastViaPortFunc.js
+++ src/webworker/broadcastViaPortFunc.js
@@ -1,56 +1,54 @@
-// //U
-// // this will be stringified. no external references allowed
-// /**
-//  * Only responsible for recieving message, then re-broadcasting that same message on the given port.
-//  * PostMessage is an expensive/timely function, so we only want to postMessage once, and have this orchestrator do all
-//  * sub-communication.
-//  *
-//  * This is needed because chrome does not allow a worker to create workers in it's thread.
-//  *
-//  * main thread -> create orchestrator. create sub workers.
-//  * main thread orchestrator -> create message channel, create subworkers, attach to port.
-//  * main thread orchestrator.postMessage -> post message via messageChannel to all subworkers.
-//  */
-// export default function broadcastViaPortFunc(){
-//   let port;//messageChannelPort
-//
-//   function initialize({port}){
-//     port = port;
-//   }
-//   function destroy(){
-//     port = undefined;
-//   }
-//
-//   function postMessageToPort(subWorkerData){
-//     if(!port){return console.error(`broadcastViaPortFunc has not been initialized with a port.`);}
-//     port.postMessage(subWorkerData);
-//   }
-//
-//   function receiveMessageFromWorker(e){
-//     let data = e.data;
-//     postMessage(data);
-//   }
-//
-//   onmessage = function(e){
-//     let data = e.data;
-//     let command = data.command;
-//     let subWorkerData = data.data;
-//     switch(command){
-//       case 'postMessageToPort':{ //e.g. update positions
-//         postMessageToPort(subWorkerData);
-//         break;
-//       }
-//       case 'intialize':{
-//         initialize(data);//port
-//         break;
-//       }
-//       case 'destroy':{
-//         destroy(data);
-//         break;
-//       }
-//       default:{
-//         console.log(`broadcastViaPortFunc did not recognize command ${command}`);
-//       }
-//     }
-//   }
-// }
+// this will be stringified. no external references allowed
+
+/**
+ * Allows us to put all the calling of other ports/workers into a worker thread.
+ * This helps us get around Chrome's limitation where web workers cannot create WebWorkers.
+ *
+ * Speeds things up considerably.
+ * 4 workers, calling postMessage to each takes ~70ms.
+ * with this func time is reduced to ~20ms.
+ *
+ * this func is the orchestrator in:
+ * https://jsfiddle.net/t463kLLw/8/
+ */
+export default function broadcastViaPortFunc(){
+  let portsArray;//messageChannelPort
+
+  function initialize({ports}){
+    portsArray = ports;
+  }
+  function destroy(){
+    portsArray = undefined;
+  }
+
+  function postMessageToAllPorts(subWorkerData){
+    if(!portsArray){return console.error(`broadcastViaPortFunc has not been initialized with ports.`);}
+    for(let i=0, len=portsArray.length; i < len; ++i){
+      let port = portsArray[i];
+      port.postMessage(subWorkerData);
+    }
+  }
+
+  onmessage = function(e){
+    let data = e.data;
+    let command = data.command;
+    let subWorkerData = data.data;
+    switch(command){
+      case 'postMessageToAllPorts':{ //e.g. update positions
+        postMessageToAllPorts(subWorkerData);
+        break;
+      }
+      case 'intialize':{
+        initialize(data);//ports
+        break;
+      }
+      case 'destroy':{
+        destroy(data);
+        break;
+      }
+      default:{
+        console.log(`broadcastViaPortFunc did not recognize command ${command}`);
+      }
+    }
+  }
+}
